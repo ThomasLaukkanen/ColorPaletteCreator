34baf1a57378e4b5ba8d8b4a571cbdf762fae273
Thomas Laukkanen <66567520+ThomasLaukkanen@users.noreply.github.com>
2022-02-11 22:50:32 +0100

    all

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('hej')
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is the begging of something great!</Text>
       <StatusBar style="auto" />
     </View>
   );
